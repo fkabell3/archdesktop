8c0a7e7649565c852426f452f334ca0b191dfbf5
Frederick Key Abell III <fkabell3@hotmail.com>
2023-06-05 03:44:33 -0500

    added /etc/skel dotfiles

---
+++ etc.skel/dotlibrewolf/defaultp.default/user.js
@@ -0,0 +1,6 @@
+user_pref("browser.download.dir", "/home/fkabell/downloads");
+user_pref("browser.download.folderList", 2);
+user_pref("browser.download.useDownloadDir", true);
+user_pref("browser.fullscreen.autohide", false);
+user_pref("layout.css.devPixelsPerPx", "1.4");
+user_pref("privacy.donottrackheader.enabled", true);
