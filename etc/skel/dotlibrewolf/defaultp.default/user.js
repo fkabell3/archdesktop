8245f17462c6b5eb6e3e64bf6bca6f7925166594
Frederick Key Abell III <fkabell3@hotmail.com>
2023-06-25 18:40:01 -0500

    ask where to save browser downloads

--- etc/skel/dotlibrewolf/defaultp.default/user.js
+++ etc/skel/dotlibrewolf/defaultp.default/user.js
@@ -1,6 +1,5 @@
 //user_pref("browser.download.dir", "$HOME/downloads");
-//user_pref("browser.download.folderList", 2);
-user_pref("browser.download.useDownloadDir", true);
+user_pref("browser.download.folderList", 2);
 user_pref("browser.fullscreen.autohide", false);
 user_pref("layout.css.devPixelsPerPx", "1.4");
 user_pref("privacy.donottrackheader.enabled", true);
